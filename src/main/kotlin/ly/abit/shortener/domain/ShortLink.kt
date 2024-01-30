5c37e98ee7fe1cf3f80b28c0bcb9dd63d84529d2
배만진 <gentlemmj@gmail.com>
2024-01-30 21:05:35 +0900

    feat: URL 형식 검사 기능 추가

--- src/main/kotlin/ly/abit/shortener/domain/ShortLink.kt
+++ src/main/kotlin/ly/abit/shortener/domain/ShortLink.kt
@@ -1,15 +1,23 @@
 package ly.abit.shortener.domain
 
 import jakarta.persistence.*
+import java.net.URI
 import java.time.LocalDateTime
 
 @Entity
-class ShortLink(
-    val url: String
-){
+class ShortLink(url: String) {
+
+    val url: String = validateUrl(url)
+
     @Id
     @Embedded
     @AttributeOverride(name = "id", column = Column(name = "shortId", unique = true))
     val shortId: ShortId = IdGenerator().generateUniqueId()
+
     val createdAt: LocalDateTime = LocalDateTime.now()
+
+    private fun validateUrl(url: String): String {
+        URI(url).toURL()
+        return url
+    }
 }
