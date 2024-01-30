c704d4625aa03384550dc47abb73ccb510e4bf8c
배만진 <gentlemmj@gmail.com>
2024-01-31 06:43:37 +0900

    feat: UUID의 일부 문자만 사용하도록 변경

--- src/main/kotlin/ly/abit/shortener/domain/IdGenerator.kt
+++ src/main/kotlin/ly/abit/shortener/domain/IdGenerator.kt
@@ -5,7 +5,10 @@ import java.util.UUID
 
 class IdGenerator {
     fun generateUniqueId(): ShortId {
-        val id = UUID.randomUUID().toString().replace("-", "")
+        val id = UUID.randomUUID().toString()
+            .replace("-", "")
+            .substring(startIndex = 2, endIndex = 12)
+
         return ShortId(id)
     }
 }
