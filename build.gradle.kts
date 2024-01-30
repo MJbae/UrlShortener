0fb59b90ec9520a26c1e065dbc4be96e880c0d70
배만진 <gentlemmj@gmail.com>
2024-01-30 10:49:46 +0900

    init project

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.1.2"
+	id("io.spring.dependency-management") version "1.1.2"
+	kotlin("jvm") version "1.8.22"
+	kotlin("plugin.spring") version "1.8.22"
+	kotlin("plugin.jpa") version "1.8.22"
+}
+
+group = "com.mj"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+	maven {
+		url = uri("https://jitpack.io")
+	}
+}
+
+val koTestVersion = "5.5.4"
+val koTestExtensionVersion = "1.1.2"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("com.mysql:mysql-connector-j")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-runner-junit5:$koTestVersion")
+	testImplementation("io.kotest:kotest-assertions-core:$koTestVersion")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:$koTestExtensionVersion")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/ly/abit/shortener/UrlShortenerApplication.kt
@@ -0,0 +1,11 @@
+package ly.abit.shortener
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlShortenerApplication
+
+fun main(args: Array<String>) {
+	runApplication<UrlShortenerApplication>(*args)
+}
