2fe971be28dee2c79d68f12be11cbdfa32f7ce16
배만진 <gentlemmj@gmail.com>
2024-01-30 22:48:18 +0900

    refactor: 트랜잭션 적용

--- src/main/kotlin/ly/abit/shortener/ShortenUrlService.kt
+++ src/main/kotlin/ly/abit/shortener/ShortenUrlService.kt
@@ -3,18 +3,21 @@ package ly.abit.shortener
 import ly.abit.shortener.domain.ShortId
 import ly.abit.shortener.domain.ShortLink
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDateTime
 
 @Service
 class ShortenUrlService(
     private val repository: ShortenUrlRepository
 ) {
+    @Transactional
     fun shorten(url: String): ShortenUrlData {
         val shortLink = ShortLink(url = url)
         val saved = repository.save(shortLink)
         return ShortenUrlData.from(saved)
     }
 
+    @Transactional(readOnly = true)
     fun findLink(id: String): ShortenUrlData {
         val shortLink = repository.findById(ShortId(id)).orElseThrow {
             ClassNotFoundException("다음의 id로 short link 조회할 수 없습니다. id: $id")
@@ -23,6 +26,7 @@ class ShortenUrlService(
         return ShortenUrlData.from(shortLink)
     }
 
+    @Transactional(readOnly = true)
     fun findOriginalLink(id: String): String {
         val shortLink = repository.findById(ShortId(id)).orElseThrow {
             ClassNotFoundException("다음의 id로 short link 조회할 수 없습니다. id: $id")
